c855a67c9e4b5c091bac03fbe15e4c253a148cea
jakepurple13 <jakerein@gmail.com>
2020-11-25 09:50:20 -0500

    - added visible scrollbar

--- templates/desktop-template/src/main/kotlin/main.kt
+++ templates/desktop-template/src/main/kotlin/main.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.foundation.lazy.LazyColumnForIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.Icon
@@ -22,6 +23,7 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.awt.Desktop
@@ -30,15 +32,28 @@ import java.net.URL
 import anime.Sources as ASources
 import manga.Sources as MSources
 
+@ExperimentalFoundationApi
 @ExperimentalKeyInput
 @ExperimentalMaterialApi
 fun main() = Window(title = "Otaku Viewer") {
     val theme = remember { mutableStateOf(darkColors()) }
     var checked by remember { mutableStateOf(false) }
+    val showDialog = remember { mutableStateOf(false) }
+    if (showDialog.value) {
+        SourceDialog(theme, MSources.values(), showDialog)
+    }
     MaterialTheme(colors = theme.value) {
         Column {
             Row(Modifier.background(theme.value.background).fillMaxWidth().padding(top = 5.dp)) {
                 Spacer(Modifier.weight(8f))
+                /*Button(
+                    modifier = Modifier
+                        .background(theme.value.background, shape = RoundedCornerShape(5.dp))
+                        .padding(5.dp)
+                        .weight(1f, true),
+                    onClick = { showDialog.value = true },
+                    colors = ButtonConstants.defaultButtonColors(backgroundColor = theme.value.surface)
+                ) { Text("Manga", style = MaterialTheme.typography.h1) }*/
                 Text(
                     if (checked) "Light" else "Dark",
                     modifier = Modifier.padding(horizontal = 5.dp).align(Alignment.CenterVertically).weight(1f),
@@ -81,6 +96,36 @@ fun main() = Window(title = "Otaku Viewer") {
     }
 }
 
+@ExperimentalMaterialApi
+@ExperimentalKeyInput
+@ExperimentalFoundationApi
+@Composable
+fun <T : GenericInfo> SourceDialog(theme: MutableState<Colors>, sources: Array<T>, showDialog: MutableState<Boolean>) =
+    Dialog({ showDialog.value = false }) {
+        MaterialTheme(colors = theme.value) {
+            LazyColumnFor(sources.toList(), modifier = Modifier.fillMaxHeight()) {
+                Card(
+                    shape = RoundedCornerShape(4.dp),
+                    border = cardBorder(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .clickable { uiViewer(it, "Test", theme) }
+                ) {
+                    Text(
+                        it.toString(),
+                        style = MaterialTheme
+                            .typography
+                            .h6
+                            .copy(textAlign = TextAlign.Center),
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
+        }
+    }
+
+@ExperimentalFoundationApi
 @ExperimentalKeyInput
 @ExperimentalMaterialApi
 fun uiViewer(info: GenericInfo, title: String, theme: MutableState<Colors>) = Window(title = "$title Viewer", centered = true) {
@@ -89,9 +134,10 @@ fun uiViewer(info: GenericInfo, title: String, theme: MutableState<Colors>) = Wi
     var progressAlpha by remember { mutableStateOf(1f) }
     var currentList by remember { mutableStateOf(info.getItems(page).toMutableList()) }
     MaterialTheme(colors = theme.value) {
-        Column(Modifier.background(theme.value.background)) {
+        Column(Modifier.background(theme.value.background).padding(5.dp)) {
             TextField(
                 value = textValue,
+                textStyle = androidx.compose.material.AmbientTextStyle.current.copy(color = theme.value.onBackground),
                 modifier = Modifier.padding(16.dp).fillMaxWidth(),
                 onValueChange = { textValue = it },
                 trailingIcon = { Icon(Icons.Filled.Search) },
@@ -101,25 +147,44 @@ fun uiViewer(info: GenericInfo, title: String, theme: MutableState<Colors>) = Wi
             )
             CircularProgressIndicator(Modifier.alpha(progressAlpha))
             progressAlpha = 0f
-            LazyColumnForIndexed(
-                currentList.filter { textValue.text in it.title },
-                modifier = Modifier.fillMaxHeight()
-            ) { index, item ->
-                if (currentList.lastIndex == index && textValue.text.isEmpty()) {
-                    onActive {
-                        //fetch more items here
-                        progressAlpha = 1f
-                        currentList = currentList.apply { addAll(info.getItems(++page)) }
-                        println("New items - $page - ${currentList.size}")
-                        progressAlpha = 0f
+            Box {
+                val listState = rememberLazyListState()
+                LazyColumnForIndexed(
+                    currentList.filter { textValue.text in it.title },
+                    state = listState,
+                    modifier = Modifier.fillMaxHeight()
+                ) { index, item ->
+                    if (currentList.lastIndex == index && textValue.text.isEmpty()) {
+                        onActive {
+                            //fetch more items here
+                            progressAlpha = 1f
+                            currentList = currentList.apply { addAll(info.getItems(++page)) }
+                            println("New items - $page - ${currentList.size}")
+                            progressAlpha = 0f
+                        }
                     }
+                    RowItem(item, theme)
+                    Divider()
                 }
-                RowItem(item, theme)
+                VerticalScrollbar(
+                    style = ScrollbarStyleAmbient.current.copy(
+                        hoverColor = theme.value.onBackground,
+                        unhoverColor = theme.value.onBackground.copy(alpha = 0.5f),
+                        hoverDurationMillis = 250
+                    ),
+                    modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
+                    adapter = rememberScrollbarAdapter(
+                        scrollState = listState,
+                        itemCount = currentList.size,
+                        averageItemSize = 37.dp
+                    )
+                )
             }
         }
     }
 }
 
+@ExperimentalFoundationApi
 @ExperimentalMaterialApi
 @Composable
 fun RowItem(item: GenericData, theme: MutableState<Colors>) {
@@ -145,6 +210,7 @@ fun RowItem(item: GenericData, theme: MutableState<Colors>) {
 const val WIDTH_DEFAULT = 360 / 2
 const val HEIGHT_DEFAULT = 480 / 2
 
+@ExperimentalFoundationApi
 @ExperimentalMaterialApi
 fun InfoLayout(item: GenericInformation, theme: MutableState<Colors>) = GlobalScope.launch {
     Window(title = item.title) {
@@ -195,20 +261,40 @@ fun TitleArea(item: GenericInformation) = Card(modifier = Modifier.padding(5.dp)
     }
 }
 
+@ExperimentalFoundationApi
 @ExperimentalMaterialApi
 @Composable
-fun ItemRows(item: GenericInformation, theme: MutableState<Colors>) = LazyColumnFor(item.rowData(), modifier = Modifier.fillMaxHeight()) {
-    Card(
-        modifier = Modifier
-            .padding(5.dp)
-            .fillMaxWidth()
-            .clickable { item.playUi(it.getItem(), it.name, theme) },
-        border = cardBorder()
-    ) {
-        Column(modifier = Modifier.padding(5.dp)) {
-            Text(it.name)
-            Text(it.uploaded, modifier = Modifier.align(Alignment.End))
+fun ItemRows(item: GenericInformation, theme: MutableState<Colors>) {
+    Box(modifier = Modifier.padding(5.dp)) {
+        val listState = rememberLazyListState()
+        val items = item.rowData()
+        LazyColumnFor(items, state = listState, modifier = Modifier.fillMaxHeight()) {
+            Card(
+                modifier = Modifier
+                    .padding(5.dp)
+                    .fillMaxWidth()
+                    .clickable { item.playUi(it.getItem(), it.name, theme) },
+                border = cardBorder()
+            ) {
+                Column(modifier = Modifier.padding(5.dp)) {
+                    Text(it.name)
+                    Text(it.uploaded, modifier = Modifier.align(Alignment.End))
+                }
+            }
         }
+        VerticalScrollbar(
+            style = ScrollbarStyleAmbient.current.copy(
+                hoverColor = theme.value.onBackground,
+                unhoverColor = theme.value.onBackground.copy(alpha = 0.5f),
+                hoverDurationMillis = 250
+            ),
+            modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
+            adapter = rememberScrollbarAdapter(
+                scrollState = listState,
+                itemCount = items.size,
+                averageItemSize = 37.dp
+            )
+        )
     }
 }
 
